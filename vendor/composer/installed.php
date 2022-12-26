b82c2ec62ca4e993e0677177d2f16a02c1c42c1c
Márcio Fernandes <marcio.fernandes@verdanatech.com>
2022-12-26 16:46:27 -0300

    :sparkles: Add Plugin

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '2.8.1',
+        'version' => '2.8.1.0',
+        'reference' => 'c8b98a9459b45113cd5258da63c501a4a0562385',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '2.8.1',
+            'version' => '2.8.1.0',
+            'reference' => 'c8b98a9459b45113cd5258da63c501a4a0562385',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
